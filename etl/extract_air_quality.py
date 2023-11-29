7e523a9d42122b0bdf71aa9b8b374f66af06afdf
dundorma <dundorma@users.noreply.github.com>
2023-11-29 21:15:53 +0700

    feat: nambah api key ges biar ga kena rate limit

--- etl/extract_air_quality.py
+++ etl/extract_air_quality.py
@@ -5,7 +5,7 @@ import requests
 def main():
     api_key = 'be11409435msha86e2ce00381450p146ce7jsn7e19a682ab57'
     api_key_2 = 'f7de1c1538msh9828d5bb47e0fafp1d417djsne7bc6e6aadfd'
-    api_key_3 = '76349352a2msh7814e54a9af5a9dp1ae144jsn004f26c6d200'
+    api_key_3 = ['8081f01e1emsh3e955d0505d9bd1p17ae86jsn4cf8e2b5cf2c','76349352a2msh7814e54a9af5a9dp1ae144jsn004f26c6d200']
 
     jams_df_drop = pd.read_csv('/opt/airflow/transform_data/traffic.csv')
     df_traffic_groupby_city = jams_df_drop.groupby('city')[['latitude','longitude']].agg({"mean"})
@@ -14,12 +14,12 @@ def main():
     url = "https://air-quality.p.rapidapi.com/history/airquality"
 
     headers = {
-	    "X-RapidAPI-Key": api_key_3,
+	    "X-RapidAPI-Key": api_key_3[0],
 	    "X-RapidAPI-Host": "air-quality.p.rapidapi.com"
     }
 
     # Loop melalui setiap baris pada DataFrame Group city
-    for index, row in df_traffic_groupby_city.iloc[:].iterrows():
+    for index, row in df_traffic_groupby_city.iloc[:20].iterrows():
         lon = str(row['longitude'].values[0])
         lat = str(row['latitude'].values[0])
         city = index
@@ -38,6 +38,33 @@ def main():
         temp_df['longitude'] = lon
         temp_df['city'] = city
         df_air_quality = pd.concat([df_air_quality, temp_df], ignore_index=True)
+
+    if len(df_traffic_groupby_city) > 20 and len(df_traffic_groupby_city) < 45:
+        headers = {
+	    "X-RapidAPI-Key": api_key_3[1],
+	    "X-RapidAPI-Host": "air-quality.p.rapidapi.com"
+        }
+
+        url = "https://air-quality.p.rapidapi.com/history/airquality"
+        for index, row in df_traffic_groupby_city.iloc[20:].iterrows():
+            lon = str(row['longitude'].values[0])
+            lat = str(row['latitude'].values[0])
+            city = index
+            print(lon, lat, city)
+            querystring = {"lon": lon, "lat": lat}
+
+            # Request untuk mendapatkan data air quality dari setiap city
+            response = requests.get(url, headers=headers, params=querystring)
+
+            air_df = response.json()['data']
+
+            print(response.json()['data'])
+
+            temp_df = pd.DataFrame(air_df)
+            temp_df['latitude'] = lat
+            temp_df['longitude'] = lon
+            temp_df['city'] = city
+            df_air_quality = pd.concat([df_air_quality, temp_df], ignore_index=True)
 
     # AIR QUALITY EXTRACT SAVE CSV
     # Making sure the directory to save the pdf exists
